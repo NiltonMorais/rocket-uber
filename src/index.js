f853ed2ed8c4e3175a0ef046abd13c7da4be5a90
Nilton Morais <niltonmorais.code@gmail.com>
2019-05-24 17:23:28 -0300

    exibindo mapa

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import MapView from 'react-native-maps';
+
+import {View} from 'react-native';
+
+const App = () => (
+    <View style={{flex: 1}}>
+        <MapView
+            style={{flex: 1}}
+            region={{
+                latitude: -27.210753,
+                longitude: -49.644183,
+                latitudeDelta: 0.0143,
+                longitudeDelta: 0.0134
+            }}
+            showsUserLocation
+            loadingEnabled
+        />
+    </View>
+);
+
+export default App;
